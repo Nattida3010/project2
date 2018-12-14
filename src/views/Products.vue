001cda0452e6a8a4ed7dda9b631a68e1e57aacb9
Puinun <Puinun.2015@gmail.com>
2018-12-15 05:51:39 +0700

    add fiter

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -19,30 +19,7 @@
            :filter="filter"
             :per-page ="pageSize"
             :current-page="pageIndex">
-             <template slot="show_details" slot-scope="row">
-      <!-- we use @click.stop here to prevent emitting of a 'row-clicked' event  -->
-      <b-button size="sm" @click.stop="row.toggleDetails" class="mr-2">
-       {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
-      </b-button>
-      <!-- In some circumstances you may need to use @click.native.stop instead -->
-      <!-- As `row.showDetails` is one-way, we call the toggleDetails function on @change -->
-      <b-form-checkbox @click.native.stop @change="row.toggleDetails" v-model="row.detailsShowing">
-        Details via check
-      </b-form-checkbox>
-    </template>
-    <template slot="row-details" slot-scope="row">
-      <b-card>
-        <b-row class="mb-2">
-          <b-col sm="3" class="text-sm-right"><b>Age:</b></b-col>
-          <b-col>{{ row.item.age }}</b-col>
-        </b-row>
-        <b-row class="mb-2">
-          <b-col sm="3" class="text-sm-right"><b>Is Active:</b></b-col>
-          <b-col>{{ row.item.isActive }}</b-col>
-        </b-row>
-        <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
-      </b-card>
-    </template> </b-table>
+            </b-table>
             
     <b-pagination  align="center"  size="md" :total-rows="products.length" v-model="pageIndex" :per-page="pageSize">
     </b-pagination>
